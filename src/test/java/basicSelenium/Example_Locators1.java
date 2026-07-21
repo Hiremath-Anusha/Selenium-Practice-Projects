e17f3a47baacb3aec21946eee9d8a1facf8639a0
Anusha Hiremath <anusha.hiremath.qa@gmail.com>
2026-07-21 11:58:31 +0200

    Locator examples on search element and signup process

---
+++ src/test/java/basicSelenium/Example_Locators1.java
@@ -0,0 +1,38 @@
+/*Open Amazon.de
+Search an item and click search button using locators*/
+
+package basicSelenium;
+
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Example_Locators1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.de");
+		driver.manage().window().maximize();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"sp-cc-rejectall-link\"]"))).click();
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("portable fan");
+		//driver.findElement(By.id("nav-search-submit-button")).click();
+		
+		
+		//To press ENTER from keyboard
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("portable fan" + Keys.ENTER); 
+		
+		
+	}
+
+}
---
+++ src/test/java/basicSelenium/Example_Locators2.java
@@ -0,0 +1,38 @@
+/*Sign up on a Demo app
+change username and password*/
+
+package basicSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Example_Locators2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.demoblaze.com/");
+		driver.manage().window().maximize();
+		
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("signin2"))).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("sign-username"))).sendKeys("randomademail@gmail.com");
+		driver.findElement(By.id("sign-password")).sendKeys("vjvbksgfvb");
+		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"signInModal\"]/div/div/div[3]/button[2]"))).click();
+		
+		//Handle JavaScript alert
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		System.out.println("Alert message: " + alert.getText());
+		alert.accept(); //clicks OK
+	}
+
+}
