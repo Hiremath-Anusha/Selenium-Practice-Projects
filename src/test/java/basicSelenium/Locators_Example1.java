bda64100b6dd8dfb4b8ec26d01c7830f4b47ea6d
Anusha Hiremath <anusha.hiremath.qa@gmail.com>
2026-07-22 11:36:26 +0200

    renamed files

--- src/test/java/basicSelenium/Locators_Example1.java
+++ src/test/java/basicSelenium/Locators_Example1.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Example_Locators1 {
+public class Locators_Example1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
