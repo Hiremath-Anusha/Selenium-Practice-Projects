1c20ba2718c79348d57ae463fd6100f53732daee
Anusha Hiremath <anusha.hiremath.qa@gmail.com>
2026-07-25 09:42:59 +0200

    more examples on dropdown

--- src/test/java/basicSelenium/Actions_Facebook_Signup.java
+++ src/test/java/basicSelenium/Actions_Facebook_Signup.java
@@ -7,24 +7,29 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Actions_KeyDown {
+public class Actions_Facebook_Signup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.de/");
+		driver.get("https://www.facebook.com");
 		driver.manage().window().maximize();
 		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-		wait.until(ExpectedConditions.elementToBeClickable(By.id("sp-cc-rejectall-link"))).click();
 		
-		WebElement search = driver.findElement(By.id("twotabsearchtextbox"));
-		Actions ac = new Actions(driver);
-		ac.keyDown(search, Keys.SHIFT).sendKeys("portable fan").keyUp(Keys.SHIFT).sendKeys(Keys.ENTER).perform();
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
