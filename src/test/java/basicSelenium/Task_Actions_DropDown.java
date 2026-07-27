532fe1f8cfadc6c1a7f869ba845382a110eddd31
Anusha Hiremath <anusha.hiremath.qa@gmail.com>
2026-07-27 21:50:42 +0200

    Task:Drop down

--- src/test/java/basicSelenium/Task_Actions_DropDown.java
+++ src/test/java/basicSelenium/Task_Actions_DropDown.java
@@ -1,9 +1,9 @@
 package basicSelenium;
 
+import java.nio.channels.Selector;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,25 +11,24 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Actions_Facebook_Signup {
+public class Task_Actions_DropDown {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com");
+		driver.get("https://the-internet.herokuapp.com/");
 		driver.manage().window().maximize();
+		
+		driver.findElement(By.linkText("Dropdown")).click();
+		WebElement dropdown = driver.findElement(By.id("dropdown"));
+		Select sc = new Select(dropdown);
+		sc.selectByIndex(2);
+		
+	
+		 
 		
 		
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
