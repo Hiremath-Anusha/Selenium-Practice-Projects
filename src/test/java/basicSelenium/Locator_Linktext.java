00f140904fdf981dccc5d10e30cab2026363ef5f
Anusha Hiremath <anusha.hiremath.qa@gmail.com>
2026-08-04 17:47:08 +0200

    xpath is changed for decline cookies

--- src/test/java/basicSelenium/Locator_Linktext.java
+++ src/test/java/basicSelenium/Locator_Linktext.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -21,8 +21,10 @@ public class Locator_Linktext {
 	driver.manage().window().maximize();
 	
 	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"facebook\"]/body/div[4]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div[2]/div"))).click();
+	//wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"facebook\"]/body/div[4]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div[2]/div"))).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Decline optional cookies']"))).click();
 	driver.findElement(By.linkText("Create new account")).click();
+	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Decline optional cookies']"))).click();
 	
 	
 	}
